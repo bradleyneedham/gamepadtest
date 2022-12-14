09aeda49857619c805aa965862b6698eae351764
Bradley Needham <bradley.needham@sony.com>
2022-12-13 16:18:00 -0800

    Add Gamepad multitouch support

--- gamepadtest.js
+++ gamepadtest.js
@@ -8,7 +8,19 @@
  */
 var haveEvents = 'GamepadEvent' in window;
 var haveWebkitEvents = 'WebKitGamepadEvent' in window;
+var last_touch0_id = 0;
+var last_touch1_id = 0;
+var n = 0;
 var controllers = {};
+
+var touchpadWidth = 1920; //DS4
+var touchpadHeight = 942; //DS4
+var touchpadScale = 4;  // 4 is well sized and presentable
+var drawSize = 5; //size of square(dot) drawn
+var drawMode = false;
+var touchpadToggle = true;
+var lastTouchpadToggle = false;
+
 var rAF = window.mozRequestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.requestAnimationFrame;
@@ -17,7 +29,8 @@ function connecthandler(e) {
   addgamepad(e.gamepad);
 }
 function addgamepad(gamepad) {
-  controllers[gamepad.index] = gamepad; var d = document.createElement("div");
+  controllers[gamepad.index] = gamepad;
+  var d = document.createElement("div");
   d.setAttribute("id", "controller" + gamepad.index);
   var t = document.createElement("h1");
   t.appendChild(document.createTextNode("gamepad: " + gamepad.id));
@@ -47,6 +60,42 @@ function addgamepad(gamepad) {
   d.appendChild(a);
   document.getElementById("start").style.display = "none";
   document.body.appendChild(d);
+
+  //create touchbar element based on size of reported touchbar
+  var touchdiv = document.createElement("div");
+  var touchCanvas = document.createElement("canvas");
+  touchCanvas.id = "touchCanvas";
+  touchdiv.className = "touchDiv";
+
+  touchCanvas.setAttribute("width", (touchpadWidth /touchpadScale));
+  touchCanvas.setAttribute("height", (touchpadHeight /touchpadScale));
+  var tctx = touchCanvas.getContext("2d");
+  tctx.fillStyle = 'rgba(0,0,0,1)';
+  tctx.strokeRect(0,0,(touchpadWidth/touchpadScale),((touchpadHeight-2)/touchpadScale));
+
+  touchdiv.appendChild(touchCanvas);
+  d.appendChild(touchdiv);
+
+
+  //create a square for RGB
+  var rgbSquareDiv = document.createElement("div");
+  var rgbSquare = document.createElement("canvas");
+  var rgbValDiv = document.createElement("div");
+  var rgbVal = document.createTextNode("R:" + drawColor.red + " G:" + drawColor.green + " B:" + drawColor.blue);
+  rgbSquareDiv.id = "rgbsquarediv";
+  rgbValDiv.id = "rgbvaldiv";
+  rgbValDiv.appendChild(rgbVal);
+  rgbSquare.id = "rgbSquare";
+  rgbSquare.setAttribute("width", 100);
+  rgbSquare.setAttribute("height", 50);
+
+  var ctx = rgbSquare.getContext('2d');
+  ctx.fillStyle = 'rgba('+drawColor.red + ',' + drawColor.green + ',' + drawColor.blue+', 1)';
+  ctx.fillRect(0, 0, 100, 50);
+  rgbSquareDiv.appendChild(rgbSquare);
+  d.appendChild(rgbValDiv);
+  d.appendChild(rgbSquareDiv);
+
   rAF(updateStatus);
 }
 
@@ -70,22 +119,46 @@ function updateStatus() {
       var b = buttons[i];
       var val = controller.buttons[i];
       var pressed = val == 1.0;
-      var touched = false;
       if (typeof(val) == "object") {
         pressed = val.pressed;
-        if ('touched' in val) {
-          touched = val.touched;
-        }
         val = val.value;
       }
       var pct = Math.round(val * 100) + "%";
       b.style.backgroundSize = pct + " " + pct;
-      b.className = "button";
       if (pressed) {
-        b.className += " pressed";
+        handleButtonRGB(controller, i, val);
+        b.className = "button pressed";
+      } else {
+        b.className = "button";
+        if(i == 17) {//touchpad toggle
+          lastTouchpadToggle = false;
+        }
       }
-      if (touched) {
-        b.className += " touched";
+    }
+    
+    if (controller.touchEvents) {
+      if (controller.touchEvents.length > 0 && controller.touchEvents[0].surfaceDimensions) {
+        touchpadWidth = controller.touchEvents[0].surfaceDimensions[0];
+        touchpadHeight = controller.touchEvents[0].surfaceDimensions[1];
+      }
+
+      var c = document.getElementById('touchCanvas');
+      ctx = c.getContext('2d');
+      if(!drawMode) {
+        ctx.clearRect(0,0,touchpadWidth/touchpadScale,touchpadHeight/touchpadScale);
+        ctx.fillStyle = 'rgba(0,0,0,1)';
+        ctx.strokeRect(0,0,(touchpadWidth/touchpadScale),((touchpadHeight-2)/touchpadScale));
+      }
+
+      for (var i = 0; i < controller.touchEvents.length; i++) {
+        var touchEvent = controller.touchEvents[i];
+        //normalize our touchpoints
+        var touchX = Math.round((touchEvent.position[0] + 1) * (touchpadWidth / 2) );
+        var touchY = Math.round((touchEvent.position[1] + 1) * (touchpadHeight / 2) );
+
+        //draw the points
+        ctx.fillStyle = 'rgba('+drawColor.red + ',' + drawColor.green + ',' + drawColor.blue+', 1)';
+        ctx.fillRect(touchX/touchpadScale, touchY/touchpadScale, drawSize, drawSize);
       }
     }
 
@@ -99,11 +172,86 @@ function updateStatus() {
   rAF(updateStatus);
 }
 
+
+var redVal = 0;
+var blueVal = 0;
+var greenVal = 0;
+var drawColor = {red:redVal, green:greenVal, blue:blueVal};
+
+function handleButtonRGB(controller, val, perc) {
+  switch(val) {
+  case 0: //x, blue
+    blueVal++;
+    if(blueVal > 255) {
+      blueVal = 255;
+    }
+    break;
+  case 1: //circle, red
+    redVal++;
+    if(redVal > 255) {
+      redVal = 255;
+    }
+    break;
+  case 2: //square, clear
+    redVal = blueVal = greenVal = 0;
+    break;
+  case 3: //triangle, green
+    greenVal++;
+    if(greenVal > 255) {
+      greenVal = 255;
+    }
+    break;
+  case 6: //L trigger
+    if (controller.vibrationActuator) {
+      controller.vibrationActuator.playEffect("dual-rumble", {duration:100, startDelay:0, strongMagnitude:perc, weakMagnitude:0});
+    }
+    break;
+  case 7: //R trigger
+    if (controller.vibrationActuator) {
+      controller.vibrationActuator.playEffect("dual-rumble", {duration:100, startDelay:0, strongMagnitude:0, weakMagnitude:perc});
+    }
+    break;
+  case 9: //Options button to clear and redraw triangle
+    var c = document.getElementById('touchCanvas');
+    ctx = c.getContext('2d');
+    ctx.clearRect(0,0,touchpadWidth/4,touchpadHeight/4);
+    ctx.fillStyle = 'rgba(0,0,0,1)';
+    ctx.strokeRect(0,0,(touchpadWidth/4),((touchpadHeight-2)/4));
+    break;
+  case 17: //touchpad
+    //logic to change the rectangle to and from "draw" mode
+        touchpadToggle = true;
+        if(touchpadToggle == true && lastTouchpadToggle == false) {
+          drawMode = !drawMode;
+          lastTouchpadToggle = touchpadToggle;
+          touchpadToggle = false;
+        }
+    break;
+  default:
+    break;
+  }
+
+  drawColor = {red:redVal, green:greenVal, blue:blueVal};
+
+  var rgbSquare = document.getElementById('rgbSquare');
+  var ctx = rgbSquare.getContext('2d');
+  ctx.fillStyle = 'rgba('+drawColor.red + ',' + drawColor.green + ',' + drawColor.blue+', 1)';
+  ctx.fillRect(0, 0, 100, 50);
+
+  //set the text nodes
+  var rgbVal = document.getElementById("rgbvaldiv")
+  rgbVal.innerHTML = "R:" + drawColor.red + " G:" + drawColor.green + " B:" + drawColor.blue + "<p>" + "set light / touchpoint color  -- draw mode: " + drawMode + "</p>";
+}
+
 function scangamepads() {
   var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
   for (var i = 0; i < gamepads.length; i++) {
-    if (gamepads[i] && (gamepads[i].index in controllers)) {
-      controllers[gamepads[i].index] = gamepads[i];
+    if (gamepads[i]) {
+      if (!(gamepads[i].index in controllers)) {
+        addgamepad(gamepads[i]);
+      } else {
+        controllers[gamepads[i].index] = gamepads[i];
+      }
     }
   }
 }
